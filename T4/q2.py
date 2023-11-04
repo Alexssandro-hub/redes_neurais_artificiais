a6a47b9193c963c199ac21dafda308e86b1038eb
Alexssandro Tabosa <71848388+Alexssandro-hub@users.noreply.github.com>
2023-11-04 18:14:40 -0300

    T4
    Trabalho_4

---
+++ T4/q2.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+class Adaline:
+    def __init__(self, learning_rate=0.1, epochs=100):
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        # Inicializa os pesos e o viés aleatoriamente
+        self.weights = np.random.rand(X.shape[1])
+        self.bias = np.random.rand()
+
+        for _ in range(self.epochs):
+            for i in range(X.shape[0]):
+                # Calcula a saída da rede
+                net_input = np.dot(X[i], self.weights) + self.bias
+                output = self.activation(net_input)
+
+                # Calcule o erro
+                error = y[i] - output
+
+                # Atualize os pesos e o viés
+                self.weights += self.learning_rate * error * X[i]
+                self.bias += self.learning_rate * error
+
+    def activation(self, x):
+        return 1 if x < 0 else 0
+
+    def predict(self, X):
+        predictions = []
+        for i in range(X.shape[0]):
+            net_input = np.dot(X[i], self.weights) + self.bias
+            output = self.activation(net_input)
+            predictions.append(output)
+        return np.array(predictions)
+
+
+X_train = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y_train = np.array([1, 0, 0, 0])  # Saída NOR correspondente
+
+
+adaline = Adaline(learning_rate=0.1, epochs=1000)
+adaline.fit(X_train, y_train)
+
+
+X_test = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+predictions = adaline.predict(X_test)
+
+
+for i, prediction in enumerate(predictions):
+    print(f'Entrada: {X_test[i]}, Saída Adaline: {prediction}')
